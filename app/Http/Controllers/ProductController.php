b6405c475c96c6fadcd0ddb90931c7f892432ced
Flavio <flavio.origgi@outlook.it>
2021-09-07 19:38:10 +0200

    creato database e scritto app/product e function product::all ma non va piu

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,27 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
-    public function comicsitem () {
+    public function products(){
+        $comics = Product::all();
 
-        $data = config('comics');
-        $comics = ['comics' => $data];
+        return view('comics', compact('comics'));
 
-        return view('comics', $comics);
+
+
     }
-
-    // @show
-
-    public function show($id) {
-
-        $data = config('comics');
-        // dd($data)
-        $comics = ['comics dettaglio' => $data[$id]];
-        // dd($comics)
-        return view('dettaglio', $comics);
-    }
+     
 
 
 }
+
+// public function comicsitem () {
+
+//     $data = config('comics');
+//     $comics = ['comics' => $data];
+
+//     return view('comics', $comics);
+// }
+
+// // @show
+
+// public function show($id) {
+
+//     $data = config('comics');
+//     // dd($data)
+//     $comics = ['comics dettaglio' => $data[$id]];
+//     // dd($comics)
+//     return view('dettaglio', $comics);
+// }
