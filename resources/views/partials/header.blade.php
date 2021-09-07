ee97d3b39e069c471b7211d2c2ff135775936300
Flavio <flavio.origgi@outlook.it>
2021-09-07 16:24:09 +0200

    prova

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+    <div class="container">
+
+        <div class="logo">
+            <img src="{{ asset('img/dc-logo.png')}} " alt="">
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href="{{ route('home') }}">Characters</a></li>
+                <li><a href="{{ route('comics') }}">Comics</a></li>
+                <li><a href="{{ route('home') }}">Movies</a></li>
+                <li><a href="{{ route('home') }}">Tv</a></li>
+                <li><a href="{{ route('home') }}">Games</a></li>
+                <li><a href="{{ route('home') }}">Collectibles</a></li>
+                <li><a href="{{ route('home') }}">Videos</a></li>
+                <li><a href="{{ route('home') }}">Fans</a></li>
+                <li><a href="{{ route('home') }}">News</a></li>
+                <li><a href="{{ route('home') }}">Shop</a></li>
+            </ul>
+        </nav>
+
+    </div>
+  </header>
